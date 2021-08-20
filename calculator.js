f49d5bad2beb617dbd226bf566480d787f8701a9
Zapzor <imp.4xlbm@aleeas.com>
2021-08-20 14:48:52 +0100

    Created functions that do basic arithmetic and added calculator buttons

---
+++ calculator.js
@@ -0,0 +1,24 @@
+//Create functions that take input and output sum depending on function used.
+
+function add(a, b) {
+   return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+ }
+ 
+ function multiply(a, b) {
+    return a * b;
+ }
+
+ function divide(a, b) {
+    return a / b;
+ } 
+
+//Create function that takes one of the above functions and uses it on a parameter
+
+function operate(a, b) {
+    
+}
+
