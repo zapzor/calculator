c334275800eb21da3ed214d62b9c8d2ec21bee83
Zapzor <imp.4xlbm@aleeas.com>
2021-08-21 11:28:24 +0100

    Calculator can now do basic calculations. Also reworked code to make it more readable (I think)

--- calculator.js
+++ calculator.js
@@ -1,115 +1,130 @@
 //Buttons and vars
+let bigArray = [];
+operator = '';
+a = 0
+b = 0
 
-one = document.querySelector('#one')
-two = document.querySelector('#two')
-three = document.querySelector('#three')
-four = document.querySelector('#four')
-five = document.querySelector('#five')
-six = document.querySelector('#six')
-seven = document.querySelector('#seven')
-eight = document.querySelector('#eight')
-nine = document.querySelector('#nine')
-zero = document.querySelector('#zero')
+one = document.querySelector('#one');
+two = document.querySelector('#two');
+three = document.querySelector('#three');
+four = document.querySelector('#four');
+five = document.querySelector('#five');
+six = document.querySelector('#six');
+seven = document.querySelector('#seven');
+eight = document.querySelector('#eight');
+nine = document.querySelector('#nine');
+zero = document.querySelector('#zero');
 
-plus = document.querySelector('#plus')
-subtract = document.querySelector('#subtract')
-multiply = document.querySelector('#multiply')
-divide = document.querySelector('#divide')
-equals = document.querySelector('#equals')
+add = document.querySelector('#add')
+subtract = document.querySelector('#subtract');
+multiply = document.querySelector('#multiply');
+divide = document.querySelector('#divide');
+equals = document.querySelector('#equals');
 
-display = document.querySelector('#display')
-divContent = document.querySelector('#content')
+display = document.querySelector('#display');
+divContent = document.querySelector('#content');
+divContent.textContent = bigArray;
 
-one.addEventListener('click', screen)
-two.addEventListener('click', screen)
-three.addEventListener('click', screen)
-four.addEventListener('click', screen)
-five.addEventListener('click', screen)
-six.addEventListener('click', screen)
-seven.addEventListener('click', screen)
-eight.addEventListener('click', screen)
-nine.addEventListener('click', screen)
-zero.addEventListener('click', screen)
+//Arithmetic buttons
+add.addEventListener('click', () => {
+    divContent.textContent = '+'
+    operator = '+'
+;})
+subtract.addEventListener('click', () => {
+    divContent.textContent = '-'
+    operator = '-'
+;})
+multiply.addEventListener('click', () => {
+    divContent.textContent = '*'
+    operator = '*'
+ ;})
+divide.addEventListener('click', () => {
+    divContent.textContent = '÷'
+    operator = '÷'
+ ;})
+ equals.addEventListener('click', operate);
 
-add.addEventListener('click', addNumber);
-subtract.addEventListener('click', subtractNumber);
-multiply.addEventListener('click', multiplyNumber);
-divide.addEventListener('click', divideNumber);
-divide.addEventListener('click', operate);
+
+ //Number buttons
+one.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 1 : b = 1;
+;})
+two.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 2 : b = 2;
+;})
+three.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 3 : b = 3;
+;})
+four.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 4 : b = 4;
+;})
+five.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 5 : b = 5;
+;})
+six.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 6 : b = 6;
+;})
+seven.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 7 : b = 7;
+;})
+eight.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 8 : b = 8;
+;})
+nine.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 9 : b = 9;
+;})
+zero.addEventListener('click', () => {
+    divContent.textContent = '1';
+    operator === '' ? a = 0 : b = 0;
+;})
+
 
 //Create functions that take input and output sum depending on function used.
 
-function addNumber(arr) {
-    return arr.reduce((total, current) => current + total, 0);
+function addNumber() {
+    return a + b
 ;}
-function subtractNumber(array) {
-   divContent.textContent = '-'
-   return array.reduce((total, current) => total - current, 0)
+function subtractNumber() {
+    return a - b
 ;}
-function multiplyNumber(array) {
-    divContent.textContent = '*'
-    return array.reduce((total, current) => total * current, 1)
+function multiplyNumber() {
+    return a * b
 ;}
-function divideNumber(array) {
-    divContent.textContent = '÷'
-    return array.reduce((total, current) => current / total, 1)
+function divideNumber() {
+    return a / b
 ;} 
 
 //Create function that takes one of the above functions and uses it on a parameter
 
-function operate(arr) {
-    addNumber(a,b)
-    subtractNumber(a,b)
-    multiplyNumber(a,b)
-    divideNumber(a,b)
-;}
+function operate() {
+    switch(operator) { 
+    case '+':
+    addNumber(bigArray)
+    divContent.textContent = addNumber();
+    return addNumber;
+    case '-':
+    subtractNumber(bigArray)
+    divContent.textContent = subtractNumber();
+    return subtractNumber;
+    case '*':
+    multiplyNumber(bigArray)
+    divContent.textContent = multiplyNumber();
+    return multiplyNumber;
+    case '/':
+    divideNumber(bigArray)
+    divContent.textContent = divideNumber();
+    return divideNumber;
+;}}
 
 //Create function that populates display when clicking buttons
 
-function screen() {
-one.addEventListener('click', () => {
-    divContent.textContent = '1' 
-    firstNumber = 1
-})
-two.addEventListener('click', () => {
-    divContent.textContent = '2' 
-    firstNumber = 2
-})
-three.addEventListener('click', () => {
-    divContent.textContent = '3'
-    firstNumber = 3
-})
-four.addEventListener('click', () => {
-    divContent.textContent = '4'
-    firstNumber = 4 
-})
-five.addEventListener('click', () => {
-    divContent.textContent = '5'
-    firstNumber = 5
-})
-six.addEventListener('click', () => {
-    divContent.textContent = '6'
-    firstNumber = 6
-})
-seven.addEventListener('click', () => {
-    divContent.textContent = '7'
-    firstNumber = 7
-})
-eight.addEventListener('click', () => {
-    divContent.textContent = '8'
-    firstNumber = 8
-})
-nine.addEventListener('click', () => {
-    divContent.textContent = '9'
-    firstNumber = 9
-})
-zero.addEventListener('click', () => {
-    divContent.textContent = '0' 
-    firstNumber = 0
-})
-}
-
 //Function that takes user input and sends it to operator
-function firstNumber(num) {
-
-}
